c38251e4e33a68b90e37ce2f0a40b29dfcd0675d
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-01-08 23:48:15 -0500

    Added navigation page

--- src/TitleViewSample/App.xaml.cs
+++ src/TitleViewSample/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TitleViewSample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
